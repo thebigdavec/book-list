a459af15355ded81f81ef3e3a84d99b61514cf6d
thebigdavec <daverbcollison@gmail.com>
2021-07-06 23:13:21 +0100

    Finished playing
    Did dark mode. Needs work.
    Added an ID to books.
    Made the X button on each book have it's own event listener.

--- app.js
+++ app.js
@@ -1,201 +1,189 @@
 // Create Book class
 class Book {
-    constructor(title, author, isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }
+  constructor(title, author, isbn) {
+    this.id = uuid.v4()
+    this.title = title
+    this.author = author
+    this.isbn = isbn
+  }
 }
-
 // Create UI class
 class UI {
-    // Add book to list function
-    addBookToList(book) {
-        // Get the book list
-        const bookList = document.querySelector("#book-list");
-        // Create tr element
-        const row = document.createElement("tr");
-        // Insert cols in the innerHTML. create more tds
-        row.innerHTML = `
+  // Add book to list function
+  addBookToList(book) {
+    // Get the book list
+    const bookList = document.querySelector('#book-list')
+    // Create tr element
+    const row = document.createElement('tr')
+    // Insert cols in the innerHTML. create more tds
+    row.innerHTML = `
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
-        <td><a href="#" class="delete">X</a></td>
-        `;
-        bookList.appendChild(row);
-    }
+        <td id="${book.id}" class="delete">X</td>
+        `
+    bookList.appendChild(row)
+    document.getElementById(book.id).addEventListener('click', this.deleteBook)
+  }
+  //show alert function
+  showAlert(message, className) {
+    //create div
+    const alertDiv = document.createElement('div')
+    //get the heading
+    const heading = document.getElementById('heading')
+    //add the classes
+    alertDiv.className = `alert ${className}`
+    //append the message
+    alertDiv.appendChild(document.createTextNode(message))
+    // add the div after
+    heading.after(alertDiv)
+    //set the timeout
+    setTimeout(function () {
+      document.querySelector('.alert').remove()
+    }, 3000)
+  }
+  // create delete Book function
+  deleteBook(e) {
+    document
+      .getElementById(e.target.id)
+      .removeEventListener('click', this.deleteBook)
+    Store.removeBookFromLocalStorage(e.target.id)
+    e.target.parentElement.remove()
+    ui.showAlert('Book removed from list', 'success')
+  }
 
-    //show alert function
-    showAlert(message, className) {
-        //create div
-        const alertDiv = document.createElement("div");
-        //get the heading
-        const heading = document.getElementById("heading");
-        //add the classes
-        alertDiv.className = `alert ${className}`;
-        //append the message
-        alertDiv.appendChild(document.createTextNode(message));
-        // add the div after
-        heading.after(alertDiv);
-        //set the timeout
-        setTimeout(function () {
-            document.querySelector(".alert").remove();
-        }, 1000);
-    }
+  removeAllBooks() {
+    document.getElementById('book-list').innerHTML = ''
+  }
 
-    // create delete Book function
-    deleteBook(target) {
-        if (target.classList.contains("delete")) {
-            target.parentElement.parentElement.remove();
-        }
-    }
-
-    removeAllBooks() {
-        document.getElementById("book-list").innerHTML = "";
-    }
-
-    // create clear Fields function
-    clearFields() {
-        document.getElementById("title").value = "";
-        document.getElementById("author").value = "";
-        document.getElementById("ISBN").value = "";
-    }
+  // create clear Fields function
+  clearFields() {
+    document.getElementById('title').value = ''
+    document.getElementById('author').value = ''
+    document.getElementById('ISBN').value = ''
+  }
 }
+const ui = new UI()
 
 // Local Storage class
 class Store {
-    //create get books function
-    static getBooks() {
-        let books;
+  //create get books function
+  static getBooks() {
+    let books
 
-        //do the usual checking in local storage and getting the item, remember to return the array
-        if (localStorage.getItem("books") === null) {
-            books = [];
-        } else {
-            books = JSON.parse(localStorage.getItem("books"));
-        }
-
-        return books;
+    //do the usual checking in local storage and getting the item, remember to return the array
+    if (localStorage.getItem('books') === null) {
+      books = []
+    } else {
+      books = JSON.parse(localStorage.getItem('books'))
     }
 
-    //create display books function
-    static displayBooks() {
-        const books = Store.getBooks();
+    return books
+  }
 
-        //loop and add every book to the list
-        books.forEach(function (book) {
-            const ui = new UI();
+  //create display books function
+  static displayBooks() {
+    const books = Store.getBooks()
 
-            ui.addBookToList(book);
-        });
-    }
+    //loop and add every book to the list
+    books.forEach(function (book) {
+      ui.addBookToList(book)
+    })
+  }
 
-    // create add book to local storage function
-    static addBookToLocalStorage(book) {
-        const books = Store.getBooks();
+  // create add book to local storage function
+  static addBookToLocalStorage(book) {
+    const books = Store.getBooks()
 
-        //push the book
-        books.push(book);
+    //push the book
+    books.push(book)
 
-        //set the item
-        localStorage.setItem("books", JSON.stringify(books));
-    }
+    //set the item
+    localStorage.setItem('books', JSON.stringify(books))
+  }
 
-    //create remove book from local storage function
-    static removeBookFromLocalStorage(isbn) {
-        const books = Store.getBooks();
+  //create remove book from local storage function
+  static removeBookFromLocalStorage(id) {
+    const books = Store.getBooks()
 
-        //loop through every books and check
-        books.forEach(function (book, index) {
-            if (book.isbn === isbn) {
-                books.splice(index, 1);
-            }
-        });
+    //loop through every books and check
+    books.forEach(function (book, index) {
+      if (book.id === id) {
+        books.splice(index, 1)
+      }
+    })
 
-        //set item to local storage
-        localStorage.setItem("books", JSON.stringify(books));
-    }
+    //set item to local storage
+    localStorage.setItem('books', JSON.stringify(books))
+  }
 
-    static removeAllBooksFromLocalStorage() {
-        localStorage.clear();
-    }
+  static removeAllBooksFromLocalStorage() {
+    localStorage.clear()
+  }
 }
 
 // DOM Load Event (event when the page loads)
-document.addEventListener("DOMContentLoaded", Store.displayBooks);
-
+document.addEventListener('DOMContentLoaded', initialSetup)
+function initialSetup() {
+  resetColorScheme()
+  Store.displayBooks()
+}
+function resetColorScheme() {
+  if (window.matchMedia('(prefers-color-scheme: no-preference').matches) return
+  if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+    document.body.classList.add('light')
+    document.getElementById('checkbox').checked = false
+  } else {
+    document.body.classList.remove('light')
+    document.getElementById('checkbox').checked = true
+  }
+}
 // Event Listener for submit
-document.getElementById("book-form").addEventListener("submit", function (e) {
-    // get form values
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const isbn = document.getElementById("ISBN").value;
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  // get form values
+  const title = document.getElementById('title').value
+  const author = document.getElementById('author').value
+  const isbn = document.getElementById('ISBN').value
 
-    // Instantiate a book
-    const book = new Book(title, author, isbn);
+  // Instantiate a book
+  const book = new Book(title, author, isbn)
 
-    // Instantiate UI
-    const ui = new UI();
+  // validation, the alert that goes when I don't fill the form
+  if (title === '' || author === '' || isbn === '') {
+    ui.showAlert('Please fill in all fields!', 'error')
+  } else {
+    // Add book to list
+    ui.addBookToList(book)
 
-    // validation, the alert that goes when I don't fill the form
-    if (title === "" || author === "" || isbn === "") {
-        ui.showAlert("Please fill in all fields!", "error");
-    } else {
-        // Add book to list
-        ui.addBookToList(book);
-
-        // Add to local Storage
-        Store.addBookToLocalStorage(book);
-
-        //show alert
-        ui.showAlert("Book added!", "success");
-        // Clear fields
-        ui.clearFields();
-    }
-
-    //prevent default
-    e.preventDefault();
-});
-
-//add event listener for the book list
-document.getElementById("book-list").addEventListener("click", function (e) {
-    const ui = new UI();
-
-    //delete book on the target
-    ui.deleteBook(e.target);
-
-    //remove from local storage, careful at the argument, get the isbn based on the target
-    Store.removeBookFromLocalStorage(
-        e.target.parentElement.previousElementSibling.textContent
-    );
+    // Add to local Storage
+    Store.addBookToLocalStorage(book)
 
     //show alert
-    ui.showAlert("Book removed!", "success");
+    ui.showAlert('Book added!', 'success')
+    // Clear fields
+    ui.clearFields()
+  }
 
-    //prevent default
-    e.preventDefault();
-});
+  //prevent default
+  e.preventDefault()
+})
 
-document.getElementById("clear-btn").addEventListener("click", function (e) {
-    const bookList = document.getElementById("book-list");
+document.getElementById('clear-btn').addEventListener('click', function (e) {
+  const bookList = document.getElementById('book-list')
 
-    if (bookList === "undefined") {
-        document.getElementById("clear-btn").disabled = true;
-    } else {
-        const ui = new UI();
+  if (bookList === 'undefined') {
+    document.getElementById('clear-btn').disabled = true
+  } else {
+    ui.removeAllBooks()
 
-        ui.removeAllBooks();
+    Store.removeAllBooksFromLocalStorage()
+  }
 
-        Store.removeAllBooksFromLocalStorage();
-    }
-
-    e.preventDefault();
-});
+  e.preventDefault()
+})
 
 // dark mode start
-
-document.getElementById("checkbox").addEventListener("change", function () {
-    document.body.classList.toggle("dark");
-});
-
+document.getElementById('checkbox').addEventListener('change', function () {
+  document.body.classList.toggle('light')
+})
 // dark mode end
